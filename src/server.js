520cece659436673cec6c4cf722f46db8891b4a9
raghav-kukreti <raghav.kukreti@gmail.com>
2020-11-26 12:29:23 +0530

    init

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+import session from 'express-session'
+
+import * as sapper from '@sapper/server';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+const app = express()
+
+app.use(session({
+    secret: 'stealthisifuwant',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(bodyParser.json())
+
+
+app.use(express.static('static'))
+
+app.use(sapper.middleware({
+    session: (req, res) => {
+        res.setHeader('cache-control', 'no-cache, no-store')
+        const sessionObj = {
+            email: req.session.email,
+            key: req.session.key,
+        }
+        req.session.error = undefined
+        return sessionObj
+    }
+}))
+app.listen({ port: PORT },
+    () => console.log(`Server ready at http://localhost:${PORT}`)
+)
