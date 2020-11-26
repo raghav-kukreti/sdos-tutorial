64cfc001e3e8c4744d41dee5ce8a07be7e70ce44
raghav-kukreti <raghav.kukreti@gmail.com>
2020-11-26 13:20:26 +0530

    update heroku push

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -47,4 +47,4 @@
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>Thank you for listening!</strong></p>
